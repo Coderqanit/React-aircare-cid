67d4498c2dfdf2044151025b829f118553e68c2a
Tolibsogir <abdulroqeebolaosebikan@gmail.com>
2024-01-11 23:13:40 +0100

    css update 2

--- src/Pages/CreateRequest.js
+++ src/Pages/CreateRequest.js
@@ -118,17 +118,53 @@ const CreateRequest = ({ data, handleChange }) => {
 
               <br />
               <div className="state-region">
-               
-                <select className="sel"
-                 type="text"
+                <select
+                  className="sel"
+                  type="text"
                   id="stateRegion"
                   name="stateRegion"
                   value={stateRegion}
-                  onChange={(e) => setstateRegion(e.target.value)}>
-                  <option>nigeria</option>
-                  <option>nigeria2</option>
-                  <option>nigeria3</option>
-                  <option>nigeria4</option>
+                  onChange={(e) => setstateRegion(e.target.value)}
+                >
+                  <option></option>
+                  <option>Abia</option>
+                  <option>Adamawa</option>
+                  <option> Akwa-Ibom </option>
+                  <option>Anambra </option>
+                  <option>Bauchi</option>
+                  <option>Bayelsa </option>
+                  <option> Benue </option>
+                  <option> Borno</option>
+                  <option> Cross River </option>
+                  <option> Delta </option>
+                  <option> Ebonyi</option>
+                  <option> Edo </option>
+                  <option> Ekiti </option>
+                  <option> Enugu </option>
+                  <option> Gombe</option>
+                  <option> Imo </option>
+                  <option> Jigawa </option>
+                  <option> Kaduna</option>
+                  <option> Kano </option>
+                  <option> Katsina</option>
+                  <option> Kebbi</option>
+                  <option> Kogi</option>
+                  <option> Kwara </option>
+                  <option> Kebbi</option>
+                  <option> Lagos</option>
+                  <option> Nasarawa</option>
+                  <option> Niger</option>
+                  <option> Ogun</option>
+                  <option> Ondo</option>
+                  <option> Osun </option>
+                  <option> Oyo</option>
+                  <option> Plateau</option>
+                  <option> Rivers </option>
+                  <option> Sokoto</option>
+                  <option> Taraba </option>
+                  <option> Yobe </option>
+                  <option> Zamfara </option>
+                  <option> Territory Federal Capital</option>
                 </select>
               </div>
 
